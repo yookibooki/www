58a107aa85bb4e25f138b51d7831a7173c77f063
John <rendogust@gmail.com>
2025-06-20 00:21:48 +0500

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+set -e
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm git base-devel
+cd /tmp
+rm -rf yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd "$HOME"
+sudo pacman -S --noconfirm zsh tmux neovim htop bat fzf ripgrep wget i3-wm i3blocks rofi dunst picom alacritty
+yay -S --noconfirm brave-bin visual-studio-code-bin
+echo "✅ Setup complete!"
